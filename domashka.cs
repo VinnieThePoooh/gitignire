f58221fe3b6fab850d498288cde91c7db14c18d5
Керим Гучигов <forsyte12316gmail.com@MacBook-Air-Kerim.local>
2023-02-12 07:25:02 +0300

    solved task4

--- domashka.cs
+++ domashka.cs
@@ -1,22 +1,24 @@
-﻿// Задача 2: Напишите программу, 
-// которая на вход принимает два числа и выдаёт, 
-// какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
-
+﻿// Задача 4: Напишите программу,
+//  которая принимает на вход три числа и выдаёт
+//   максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 
 System.Console.WriteLine("Введите первое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1>number2)
+System.Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (max < number2)
 {
-    System.Console.Write("Максимальным является ");
-    System.Console.WriteLine(number1);
+    max = number2 ; 
 }
-else
+if (max < number3)
 {
-   System.Console.Write("Максимальным является ");
-    System.Console.WriteLine(number2);  
+    max = number3 ; 
 }
+System.Console.Write("Наибольшим является ");
+System.Console.WriteLine(max);
